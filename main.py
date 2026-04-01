0f0489904bb30351203732d4ddf27ebc8ed298fa
R3alHumanBe1ng <csadowsk@outlook.com>
2026-04-01 14:03:54 -0700

    google

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+from google import genai
+
+client = genai.Client(api_key='')
+
 @app.route("/")
 def home():
-    return index.html
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def handle_post():
+    subject = request.form['field1']
+    background = request.form['field2']
+    text = request.form['field3']
+
+    if subject == '' | background == '' | text == '':
+        return render_template('index.html')
+    
+    
