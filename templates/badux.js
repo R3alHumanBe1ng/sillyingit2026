a075764c512da6e1370638c6c15f67f58b73f5a5
Syfirry <syfirry@gmail.com>
2026-04-01 13:31:11 -0700

    a bunch of shit

---
+++ templates/badux.js
@@ -0,0 +1,48 @@
+const STEP = 400;
+
+window.addEventListener("wheel", (e) => {
+    e.preventDefault();
+}, { passive: false });
+
+window.addEventListener(
+    "touchmove",
+    (e) => {
+        e.preventDefault();
+    },
+    { passive: false }
+);
+
+window.addEventListener("mousedown", (e) => {
+  if (e.button === 1) e.preventDefault();
+});
+
+  window.addEventListener("keydown", (e) => {
+    const keysToBlock = [
+        "ArrowUp",
+        "ArrowDown",
+        "ArrowLeft",
+        "ArrowRight",
+        "PageUp",
+        "PageDown",
+        "Home",
+        "End",
+        " "
+    ];
+    switch (e.key) {
+      case "w":
+        window.scrollBy({top: STEP, behavior: "smooth"});
+        break;
+      case "s":
+        window.scrollBy({top: -STEP, behavior: "smooth"});
+        break;
+      default:
+        e.preventDefault();
+        return;
+    }
+    if (keysToBlock.includes(e.key)) {
+        e.preventDefault();
+    }
+    e.preventDefault();
+  });
+
+  window.scrollBy({ top: document.body.scrollHeight, behavior: "smooth" });
